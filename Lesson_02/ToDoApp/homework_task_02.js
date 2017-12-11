69b824b5488c6cec388016b0685ec153c0020ebb
Julia <j.may2501@gmail.com>
2017-12-11 20:30:51 +0100

    ToDoApp with Ajax

--- Lesson_02/ToDoApp/homework_task_02.js
+++ Lesson_02/ToDoApp/homework_task_02.js
@@ -1,3 +1,31 @@
+function loadTasks() {
+  $.getJSON('http://sealcode.org:8082/api/v1/resources/task')
+  .done( function(data){
+    $.each(data, function(key, val) {
+      var newValue = val.body.title;
+      var button = $('<button type="button" class="delbutton">Delete</button>');
+      button.on('click', function() {
+        $(this).parent().remove();
+      });
+      var taskDone = $('<input type="checkbox"><label></label><br>');
+      taskDone.on('change', function() {
+        if ($(this).is(':checked')) {
+          $(this).next('label').text('Task done');
+        }
+        else {
+          $(this).next('label').text('');
+        }
+      });
+      var newTask = $('<p>' + newValue + '</p>');
+      newTask.append(taskDone);
+      newTask.append(button);
+      $('div').append(newTask);
+    });
+  });
+}
+
+loadTasks();
+
 var date = $('#date');
 date.text(new Date().toLocaleDateString());
 
@@ -32,6 +60,26 @@ function addTask() {
  newTask.append(button);
  $('div').append(newTask);
  $('#newtask').val('');
+
+ $.ajax({
+  type: "POST",
+  url: 'http://sealcode.org:8082/api/v1/resources/task',
+  dataType: 'application/json',
+  data: JSON.stringify({
+    "title": newValue,
+    "is_done": false
+  }),
+  complete: function() {
+  alert('Completed!');
+  },
+  success: function() {
+    alert('Successfully uploaded!');
+  },
+  fail: function() {
+  alert('Failed!');
+  }
+});
+
 }
 else {
   alert('You cannot add an empty task!!!');
